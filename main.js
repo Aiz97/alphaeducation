99ca8b3ae72567dd830cda624c3d82bf06468769
Aizat <s.aizat21@gmail.com>
2022-07-18 14:16:11 +0600

    feat: mobile responsive

--- main.js
+++ main.js
@@ -1,13 +1,20 @@
 $(document).ready(function(){
     $('.customer-stories').slick({
-        slidesToShow: 3,
+        slidesToShow: 1,
         slidesToScroll: 1,
         autoplay: true,
         autoplaySpeed: 1500,
         arrows: false,
         dots: false,
         pauseOnHover: false,
+        mobileFirst:true,
         responsive: [{
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3
+            }
+        },
+            {
             breakpoint: 768,
             settings: {
                 slidesToShow: 1
@@ -17,6 +24,28 @@ $(document).ready(function(){
             settings: {
                 slidesToShow: 1
             }
+        }, {
+            breakpoint: 390,
+            settings: {
+                slidesToShow: 1
+            }
         }]
     });
 });
+
+const burgerIcon = document.querySelector('.burger-icon');
+const burgerMenu = document.querySelector('.nav-list');
+const body = document.body;
+
+burgerIcon.addEventListener('click', function(e){
+    this.classList.toggle('open');
+    burgerMenu.classList.toggle('open');
+    body.classList.toggle('no-scroll');
+})
+burgerMenu.addEventListener('click', function(e){
+    if (e.target.matches('.nav-link')) {
+        burgerIcon.classList.remove('open');
+        burgerMenu.classList.remove('open');
+        body.classList.remove('no-scroll');
+    }
+})
